bad6613ee6861d89eb708236ae077a94920b6230
Vince Tse <thelazyenginerd@gmail.com>
2016-09-03 16:45:12 -0400

    more tests (#7)

--- s3cache/tests/test_s3cache.py
+++ s3cache/tests/test_s3cache.py
@@ -97,46 +97,49 @@ class TestS3Cache(unittest.TestCase):
         f = s3.open(object_name, "r")
 
     def testFileCreateAndDelete(self):
-        s3 = self.bucketFactory()
-        object_name = "/foo/bar.txt"
-        content = "content"
-        # Create an object
-        f = s3.open(object_name, "w")
-        f.write(content)
-        bytes_written = f.close()
-        self.assertEqual(bytes_written, len(content))
-        self.assertTrue(s3.object_exists(object_name))
-        # then delete it
-        self.assertTrue(s3.remove_object(object_name))
-        self.assertFalse(s3.object_exists(object_name))
-        # Create an object again
-        f = s3.open(object_name, "w")
-        f.write(content)
-        bytes_written = f.close()
-        self.assertEqual(bytes_written, len(content))
-        self.assertTrue(s3.object_exists(object_name))
-        # Revove using file handle
-        self.assertTrue(f.remove())
-        self.assertFalse(s3.object_exists(object_name))
+        for caching in (True, False):
+            s3 = self.bucketFactory()
+            s3.set_caching(caching)
+            object_name = "/foo/bar.txt"
+            content = "content"
+            # Create an object
+            f = s3.open(object_name, "w")
+            f.write(content)
+            bytes_written = f.close()
+            self.assertEqual(bytes_written, len(content))
+            self.assertTrue(s3.object_exists(object_name))
+            # then delete it
+            self.assertTrue(s3.remove_object(object_name))
+            self.assertFalse(s3.object_exists(object_name))
+            # Create an object again
+            f = s3.open(object_name, "w")
+            f.write(content)
+            bytes_written = f.close()
+            self.assertEqual(bytes_written, len(content))
+            self.assertTrue(s3.object_exists(object_name))
+            # Revove using file handle
+            self.assertTrue(f.remove())
+            self.assertFalse(s3.object_exists(object_name))
 
     def testTwoHandlesToSameFile(self):
-        s3 = self.bucketFactory()
-        object_name = "/foo/bar.txt"
-        content = "content"
-        # Create an object
-        f1 = s3.open(object_name, "w")
-        f1.write(content)
-        bytes_written = f1.close()
-        self.assertEqual(bytes_written, len(content))
-        self.assertTrue(f1.exists())
-        self.assertTrue(s3.object_exists(object_name))
-        f2 = s3.open(object_name, "r")
-        self.assertTrue(f2.exists())
-        self.assertTrue(f2.remove())
-        # now both handles will should that the file doesn't exist
-        self.assertFalse(s3.object_exists(object_name))
-        self.assertFalse(f1.exists())
-        self.assertFalse(f2.exists())
+        for caching in (True, False):
+            s3 = self.bucketFactory()
+            object_name = "/foo/bar.txt"
+            content = "content"
+            # Create an object
+            f1 = s3.open(object_name, "w")
+            f1.write(content)
+            bytes_written = f1.close()
+            self.assertEqual(bytes_written, len(content))
+            self.assertTrue(f1.exists())
+            self.assertTrue(s3.object_exists(object_name))
+            f2 = s3.open(object_name, "r")
+            self.assertTrue(f2.exists())
+            self.assertTrue(f2.remove())
+            # now both handles will should that the file doesn't exist
+            self.assertFalse(s3.object_exists(object_name))
+            self.assertFalse(f1.exists())
+            self.assertFalse(f2.exists())
 
     def testLocallyCached(self):
         s3 = self.bucketFactory()
@@ -170,58 +173,61 @@ class TestS3Cache(unittest.TestCase):
         self.assertFalse(f.cached())
 
     def testCreateAndAppendToFile(self):
-        s3 = self.bucketFactory()
-        # Turn on caching
-        s3.set_caching(True)
-        object_name = "new-file.txt"
-        content = "content"
-        # write a file
-        f = s3.open(object_name, "w")
-        f.write(content)
-        bytes_written = f.close()
-        self.assertEqual(bytes_written, len(content))
-        self.assertTrue(s3.object_exists(object_name))
-        # now append to it
-        f = s3.open(object_name, "a")
-        f.write(content)
-        bytes_written = f.close()
-        self.assertEqual(bytes_written, len(content) * 2)
-        self.assertTrue(s3.object_exists(object_name))
+        for caching in (True, False):
+            s3 = self.bucketFactory()
+            # Toggle caching
+            s3.set_caching(caching)
+            object_name = "new-file.txt"
+            content = "content"
+            # write a file
+            f = s3.open(object_name, "w")
+            f.write(content)
+            bytes_written = f.close()
+            self.assertEqual(bytes_written, len(content))
+            self.assertTrue(s3.object_exists(object_name))
+            # now append to it
+            f = s3.open(object_name, "a")
+            f.write(content)
+            bytes_written = f.close()
+            self.assertEqual(bytes_written, len(content) * 2)
+            self.assertTrue(s3.object_exists(object_name))
 
     def testOverWriteExistingFile(self):
-        s3 = self.bucketFactory()
-        # Turn on caching
-        s3.set_caching(True)
-        object_name = "new-file.txt"
-        content = "content"
-        # write a file
-        f = s3.open(object_name, "w")
-        f.write(content)
-        bytes_written = f.close()
-        self.assertEqual(bytes_written, len(content))
-        self.assertTrue(s3.object_exists(object_name))
-        f = s3.open(object_name, "r")
-        line = f.readline()
-        f.close()
-        self.assertEqual(line, content)
-        # now append to it
-        alternative_content = "Mary had a little lamb"
-        self.assertNotEqual(content, alternative_content)
-        f = s3.open(object_name, "w")
-        f.write(alternative_content)
-        bytes_written = f.close()
-        self.assertEqual(bytes_written, len(alternative_content))
-        self.assertTrue(s3.object_exists(object_name))
-        f = s3.open(object_name, "r")
-        line = f.readline()
-        f.close()
-        self.assertEqual(line, alternative_content)
+        for caching in (True, False):
+            s3 = self.bucketFactory()
+            # Toggle caching
+            s3.set_caching(caching)
+            object_name = "new-file.txt"
+            content = "content"
+            # write a file
+            f = s3.open(object_name, "w")
+            f.write(content)
+            bytes_written = f.close()
+            self.assertEqual(bytes_written, len(content))
+            self.assertTrue(s3.object_exists(object_name))
+            f = s3.open(object_name, "r")
+            line = f.readline()
+            f.close()
+            self.assertEqual(line, content)
+            # now append to it
+            alternative_content = "Mary had a little lamb"
+            self.assertNotEqual(content, alternative_content)
+            f = s3.open(object_name, "w")
+            f.write(alternative_content)
+            bytes_written = f.close()
+            self.assertEqual(bytes_written, len(alternative_content))
+            self.assertTrue(s3.object_exists(object_name))
+            f = s3.open(object_name, "r")
+            line = f.readline()
+            f.close()
+            self.assertEqual(line, alternative_content)
 
     def testCreateAndDownloadFile(self):
         # Write a file out file
         object_name = "myfile1.txt"
         content = "content"
         s3 = self.bucketFactory()
+        s3.set_caching(True)
         f = s3.open(object_name, "w")
         f.write(content)
         bytes_written = f.close()
@@ -236,9 +242,11 @@ class TestS3Cache(unittest.TestCase):
         # Now manually remove the file from local cache
         os.unlink(local_filename)
         self.assertFalse(os.path.exists(local_filename))
-        # Open the file to read and see that it is downloaded from the bucket
+        # Open the file to read and see that it is downloaded
+        # from the bucket
         f = s3.open(object_name, "r")
         line = f.readline()
         self.assertEqual(line, content)
         # Remove file
-        s3.remove_object(object_name)
+        self.assertTrue(s3.remove_object(object_name))
+        self.assertFalse(s3.object_exists(object_name))
